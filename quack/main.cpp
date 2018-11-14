19ff4fbcb77712c2c27b44960c017cd4de30ecc7
Arturo <arttu.gz94@hotmail.com>
2018-11-13 19:39:51 -0600

    Restored the original version but keeping audiere for the sound playing

--- quack/main.cpp
+++ quack/main.cpp
@@ -8,8 +8,6 @@
 #include <Windows.h>
 #include <mmsystem.h>
 
-const int MAX_WORD_SIZE = 30;
-
 using namespace std;
 
 HHOOK g_Hook;
@@ -17,11 +15,6 @@ HHOOK k_Hook;
 HANDLE g_evExit;
 
 HCURSOR hcur;
-
-char keyboardInputBuffer[MAX_WORD_SIZE];
-int keyboardInputBufferIndex;
-
-vector<string> vWords;
 
 LRESULT CALLBACK LowLevelKeyboardProc(int code, WPARAM wParam, LPARAM lParam)
 {
@@ -42,8 +35,7 @@ LRESULT CALLBACK LowLevelKeyboardProc(int code, WPARAM wParam, LPARAM lParam)
 			break;
 		}
 
-		const MSLLHOOKSTRUCT *p =
-			reinterpret_cast<const MSLLHOOKSTRUCT*>(lParam);
+		const MSLLHOOKSTRUCT *p = reinterpret_cast<const MSLLHOOKSTRUCT*>(lParam);
 		//cout << msg << " - [" << p->pt.x << ',' << p->pt.y << ']' << endl;
 
 		static bool keydown = false;
@@ -56,38 +48,12 @@ LRESULT CALLBACK LowLevelKeyboardProc(int code, WPARAM wParam, LPARAM lParam)
 
 		if (keydown)
 		{
-			keyboardInputBuffer[keyboardInputBufferIndex] = static_cast<char>(p->pt.x);
-			keyboardInputBufferIndex = (keyboardInputBufferIndex + 1) % MAX_WORD_SIZE;
-
-			//system("cls");
-			for (int i = 0; i < MAX_WORD_SIZE; i++)
-			{
-				cout << keyboardInputBuffer[i] << " ";
-			}
-
-			if (
-				p->pt.x == 32 || //spacebar
-				p->pt.x == 13 || //return-enter
-				p->pt.x == 8 || //backspace
-				p->pt.x == 160 || //shift
-				p->pt.x == 161 || //shift
-				p->pt.x == 162    //ctrl
-				//p->pt.x == 66 || //b
-				//p->pt.x == 65 || //a
-				//p->pt.x == 82 || //r
-				//p->pt.x == 75 || //k
-				//p->pt.x == 69 || //e
-				//p->pt.x == 73 || //i
-				//p->pt.x == 79 || //o
-				//p->pt.x == 85
-				)   //u
-			{
-				sndPlaySound(TEXT("data/bark.wav"), SND_ASYNC);
-			}
+			//sndPlaySound(TEXT("data/quack.wav"), SND_ASYNC);
+			PlaySound(TEXT("data/quack.wav"), NULL, SND_ASYNC);
 		}
-		else if (!keyup)
+		else if(keyup)
 		{
-			//PlaySound(NULL, NULL, SND_ASYNC);
+			PlaySound(NULL, NULL, SND_ASYNC);
 		}
 	}
 	return CallNextHookEx(g_Hook, code, wParam, lParam);
@@ -142,69 +108,31 @@ int main()
 	g_evExit = CreateEvent(0, TRUE, FALSE, 0);
 	if (!g_evExit)
 	{
-		//cerr << "CreateEvent failed, le = " << GetLastError() << endl;
+		cerr << "CreateEvent failed, le = " << GetLastError() << endl;
 		return 1;
-	}//if
-
-	//string line;
-	//ifstream mFile("ini.txt");
-	//if(!mFile.is_open())
-	//{
-	//	MessageBoxA(nullptr, (LPCSTR)"ini file could not be opened, closing quacker", (LPCSTR)"Error loading ini.txt", MB_OK);
-	//	return 1;
-	//}
-	//else
-	//{
-	//	while (getline(mFile, line))
-	//	{
-	//		int endOfFirstFind;
-	//		endOfFirstFind = line.find(" ");
-	//		string tempWord;
-	//		string tempFilename;
-	//		for (int i = 0; i < endOfFirstFind; i++)
-	//		{
-	//			tempWord.push_back(line[i]);
-	//		}
-	//		cout << tempWord << " ";
-	//		for (int i = (int)endOfFirstFind + 1; i < line.size(); i++)
-	//		{
-	//			tempFilename.push_back(line[i]);
-	//		}
-	//		cout << tempFilename << endl;
-	//		CreateSound(tempFilename, tempWord);
-	//		//CreatefmodSound(tempFilename, tempWord);
-	//	}
-	//	mFile.close();
-	//}
+	}
 
 	g_Hook = SetWindowsHookEx(WH_MOUSE_LL, &LowLevelMouseProc, GetModuleHandle(0), 0);
 	if (!g_Hook)
 	{
-		//SystemParametersInfo(SPI_SETCURSORS, 0, NULL, 0);
-		//cerr << "SetWindowsHookEx() failed, le = " << GetLastError() << endl;
+		SystemParametersInfo(SPI_SETCURSORS, 0, NULL, 0);
+		cerr << "SetWindowsHookEx() failed, le = " << GetLastError() << endl;
 		return 1;
-	}//if
+	}
 
 	k_Hook = SetWindowsHookEx(WH_KEYBOARD_LL, &LowLevelKeyboardProc, GetModuleHandle(0), 0);
 	if (!k_Hook)
 	{
-		//SystemParametersInfo(SPI_SETCURSORS, 0, NULL, 0);
+		SystemParametersInfo(SPI_SETCURSORS, 0, NULL, 0);
 		return 1;
 	}
 
 	//cout << "Press both left and right mouse buttons to exit..." << endl;
 
-	//FreeConsole();
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
 
-	for (int i = 0; i < MAX_WORD_SIZE; i++)
-	{
-		keyboardInputBuffer[i] = NULL;
-	}
-	keyboardInputBufferIndex = 0;
-
-	//hcur = LoadCursorFromFile(L"C:\\windows\\cursors\\duck.cur");
-	//hcur = LoadCursorFromFile(L"data/duck.cur");
-	//SetSystemCursor(hcur, 32512/*OCR_NORMAL*/);
+	hcur = LoadCursorFromFile(L"data/duck.cur");
+	SetSystemCursor(hcur, 32512/*OCR_NORMAL*/);
 
 	MSG msg;
 
@@ -221,20 +149,24 @@ int main()
 		{
 			// there are messages to process, eat em up
 			continue;
-		}//if
+		}
 		else
 		{
 			SystemParametersInfo(SPI_SETCURSORS, 0, NULL, 0);
 			// assume g_evExit is signaled
 			break;
-		}//else
-	}//while
+		}
+	}
 
 	//cout << "Exiting..." << endl;
-	//SystemParametersInfo(SPI_SETCURSORS, 0, NULL, 0);
+	SystemParametersInfo(SPI_SETCURSORS, 0, NULL, 0);
 
 	UnhookWindowsHookEx(g_Hook);
 	UnhookWindowsHookEx(k_Hook);
 	CloseHandle(g_evExit);
+
 	return 0;
 }//main
+
+
+//q u a c k  
